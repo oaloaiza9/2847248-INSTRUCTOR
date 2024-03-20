823ce74637d7c06efac2196046e12a513d416875
Oscar Andres Loaiza <oaloaiza9@misena.edu.co>
2024-03-20 13:10:34 -0500

    Ajustando estructura repositorio Instructor

---
+++ 02 - Ejemplos/01 - Introduccion/Variables.java
@@ -0,0 +1,25 @@
+public class Variables{
+	public static void main(String[] args){
+
+		// Variables de tipo Primitivo
+		byte edad = 18;
+		short turno = 250;
+		int cedula = 1088003492;
+		long telefono = 3203454839l;
+		float nota = 4.5f;
+		double latitud = 29.939402939;
+		boolean estado = true;
+		char simbolo = '@';
+		String nombres = "Oscar Andres";
+
+		System.out.println("Edad: " + edad);
+		System.out.println("Turno: " + turno);
+		System.out.println("Cedula: " + cedula);
+		System.out.println("Telefono: " + telefono);
+		System.out.println("Nota: " + nota);
+		System.out.println("Latitud: " + latitud);
+		System.out.println("Estado: " + estado);
+		System.out.println("Simbolo: " + simbolo);
+		System.out.println("Nombres: " + nombres);
+	}
+}
